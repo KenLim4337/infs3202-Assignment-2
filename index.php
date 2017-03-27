d0dd6a88fefb953ee12089a3bcf2980944e3b9d6
Daniel Fraser <danielfraser1205@gmail.com>
2017-03-28 08:32:32 +1000

    file stub creation

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+
+ 	<title>Weight Memory</title>
+ 	
+    <script src="js/jquery.js"></script>
+
+    <script src="https://use.fontawesome.com/daef70889d.js"></script>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">
+
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+</head>
+<body>
+
+	
+
+</body>
+</html>
